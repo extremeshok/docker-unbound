4fc47ac742ff138f4597bdde6ba800c6f8abbc46
eXtremeSHOK <admin@extremeshok.com>
2019-08-21 03:54:10 +0200

    verbose on ipv6 status

--- README.md
+++ README.md
@@ -7,13 +7,13 @@ Unbound is a validating, recursive, and caching DNS resolver.
 View **docker-compose-sample.yml** in the source repository for usage
 
 # features
-Alpine latest
-Log to console(tty)
-Remote-control enabled
-Secured for lan
-Highly optimized
-Memory caching
-Sane min-TTL and max-TTL
+- Alpine latest
+- Log to console(tty)
+- Remote-control enabled
+- Secured for lan
+- Highly optimized
+- Memory caching
+- Sane min-TTL and max-TTL
 
 # environment
 UNBOUND_ENABLE_IPV6=false (set to true to enable ipv6 support)
--- rootfs/docker-entrypoint.sh
+++ rootfs/docker-entrypoint.sh
@@ -28,9 +28,11 @@ chown -R unbound /etc/unbound
 if [ "$XS_ENABLE_IPV6" == "yes" ] || [ "$XS_ENABLE_IPV6" == "true" ] || [ "$XS_ENABLE_IPV6" == "on" ] || [ "$XS_ENABLE_IPV6" == "1" ] ; then
   sed -i "s/do-ip6: no/do-ip6: yes/g" /etc/unbound/unbound.conf
   sed -i "s/prefer-ip6: no/prefer-ip6: yes/g" /etc/unbound/unbound.conf
+  echo "IPv6 Enabled"
 else
   sed -i "s/do-ip6: yes/do-ip6: no/g" /etc/unbound/unbound.conf
   sed -i "s/prefer-ip6: yes/prefer-ip6: no/g" /etc/unbound/unbound.conf
+  echo "IPv6 Disabled"
 fi
 
 exec /usr/sbin/unbound
